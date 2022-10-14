19709582fdd3f944acb0c9d2279752be8d92b2de
Jenaide <jenaidesibolie@gmail.com>
2022-10-14 16:35:52 -0700

    Task 8 heaxadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char awe;
+
+	int h;
+
+	awe = 'a';
+	h = 0;
+	while
+		(h < 10) {
+			putchar(h + '0');
+			h++;
+		}
+	while
+		(awe <= 'f') {
+			putchar(awe);
+			awe++;
+		}
+	putchar('\n');
+	return (0);
+}
