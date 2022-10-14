8cb8f154551898b62be9c43c59af62723b18636c
Jenaide <jenaidesibolie@gmail.com>
2022-10-14 16:18:58 -0700

    Task 7 smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char awe;
+
+	awe = 'z';
+	while
+		(awe >= 'a') {
+			putchar(awe);
+			awe--;
+		}
+	putchar('\n');
+	return (0);
+}
