742b26c417e9dfbbbfcda30ae94a8d603a3bfb02
Jenaide <jenaidesibolie@gmail.com>
2022-10-18 15:32:19 -0700

    Task 9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - check main
+ * prints 9 times table
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if ((z / 10) == 0)
+			{
+				if (y != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (y == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
