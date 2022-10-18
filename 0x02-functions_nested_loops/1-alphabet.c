f2f8cba9114156b1245837ea67c7e7846879db7c
Jenaide <jenaidesibolie@gmail.com>
2022-10-18 13:27:08 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - will print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char da;
+
+	for (da = 'a'; da <= 'z'; da++)
+		_putchar(da);
+	_putchar('\n');
+}
