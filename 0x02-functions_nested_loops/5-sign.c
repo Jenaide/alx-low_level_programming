48e08a1e6ad944f8733bddd1cabbbe825c3a7964
Jenaide <jenaidesibolie@gmail.com>
2022-10-18 14:25:27 -0700

    Task 5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - check main
+ * @n: print the number of the sign
+ * Return: 1 if the greater than 0(positive)
+ * 0 is zero
+ * -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
