5aabe924ed7fa7143c76a0bc85d64fa30d81b2d2
Jenaide <jenaidesibolie@gmail.com>
2022-10-18 15:54:04 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - check main
+ * @n: integer
+ * print from n to 98
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
