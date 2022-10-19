0c5e425bb8e5d3f1ce9e64ce67879c6263a7292d
Jenaide <jenaidesibolie@gmail.com>
2022-10-19 12:03:56 -0700

    task0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if int is post or neg
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int k;
+
+	k = 0;
+	positive_or_negative(k);
+
+	return (0);
+}
