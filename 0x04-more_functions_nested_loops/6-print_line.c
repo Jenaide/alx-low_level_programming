ba9b27603e678519eabac263a8155069fd2b051c
Jenaide <jenaidesibolie@gmail.com>
2022-10-20 14:37:40 -0700

    6. The shortest distance between two points is a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line using '-'
+ * @n: input integer
+ * Return: always 0
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+			_putchar('-');
+	}
+
+	_putchar('\n');
+}
