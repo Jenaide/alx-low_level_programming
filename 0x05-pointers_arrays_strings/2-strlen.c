f1e5becfa09877ef5c0e33b95d5980b9b47d618e
Jenaide <jenaidesibolie@gmail.com>
2022-10-24 12:45:11 -0700

    Task 2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string type input
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		lenght++;
+	return (lenght);
+}
