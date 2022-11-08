1f4d21a8755873b111f58bb89b384a38f7ce2e73
Jenaide <jenaidesibolie@gmail.com>
2022-11-08 12:09:03 -0800

    Task 2 a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -45,7 +45,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (x = 0; *(s1 + x) != '\0'; x++;
+	for (x = 0; *(s1 + x) != '\0'; x++);
 		*(des + x) = *(s1 + x);
 
 	for (y = 0; *(s2 + y) != '\0'; y++)
