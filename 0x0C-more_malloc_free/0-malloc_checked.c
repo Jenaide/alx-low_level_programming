5990b8f7278de9e187002f0e0509006f331995fe
Jenaide <jenaidesibolie@gmail.com>
2022-11-10 11:00:42 -0800

    created a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocated memory using malloc
+ * @b: unsigned integer
+ * Return: a pointer to allocate memory or NULL on failure
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+
+	if (x == NULL)
+		exit(98);
+
+	return (x);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+assignment 0x0C-more_malloc_free
