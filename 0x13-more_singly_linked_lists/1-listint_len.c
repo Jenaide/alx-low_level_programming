ef86d0c0be349cda1a478159db5cd2681bd0b8f9
Jenaide <jenaidesibolie@gmail.com>
2022-11-28 10:41:30 -0800

    fixed a bug in listint_len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -17,23 +17,4 @@ size_t listint_len(const listint_t *h)
 		nodes++;
 	}
 	return (nodes);
-}#include "lists.h"
-#include <stdio.h>
-
-/**
- * listint_len - function that returns the number of elements in
- * a linked list_t list
- * @h: pointer to listint_t struct
- * Return: num of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	size_t nodes = 0;
-
-	while (h)
-	{
-		h = h->next;
-		nodes++;
-	}
-	return (nodes);
 }
